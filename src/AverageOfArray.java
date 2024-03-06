136a8aff729c369dc7b681afbdce73c8cf33aac2
Abdullah KAYA <abdullahky0409@gmail.com>
2024-03-06 19:54:05 +0300

    .

--- src/AverageOfArray.java
+++ src/AverageOfArray.java
@@ -2,10 +2,10 @@ import java.util.Arrays;
 
 public class AverageOfArray {
     public static void main(String[] args) {
-        int [] list = {0,1,2,3,4,5,6};
+        int [] list = {7,1,2,3,4,5,6};
         double total=0;
         for (int i=0;i< list.length; i++){
-            total+=i;
+            total+=list[i];
         }
         double averaga= total/list.length;
         System.out.print(averaga);
